1aae4bf13746d593f5bcd17b5bd07473a35fa159
Charlotte Lim <s10258748@connect.np.edu.sg>
2024-11-16 20:10:38 +0800

    removed userlogout

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -40,15 +40,4 @@ const userLoginController = async (req, res) => {
     }
 };
 
-// User Logout
-const userLogoutController = (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            console.error('Error during logout:', err);
-            return res.status(500).json({ message: 'Logout failed' });
-        }
-        res.status(200).json({ message: 'Logout successful' });
-    });
-};
-
 export { createUserController, userLoginController, userLogoutController };
