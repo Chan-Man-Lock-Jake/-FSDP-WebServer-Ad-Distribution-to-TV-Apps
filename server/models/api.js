602903765a2b7416dfe6d0a5b645af321f722f0e
Charlotte Lim <s10258748@connect.np.edu.sg>
2024-11-17 17:22:14 +0800

    Deleted files that are not required

--- server/models/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: 'http://localhost:3000/api', // Your Node.js backend URL
-});
-
-export const signupUser = (userData) => API.post('/signup', userData);
-
-export default API;
